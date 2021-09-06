bdff632896933b685fd861516f344e55ee03cb0e
Kento Furui <kentofurui@gmail.com>
2021-09-06 21:05:22 +0900

    responsive table

--- resources/views/taxon/_table.blade.php
+++ resources/views/taxon/_table.blade.php
@@ -1,32 +1,34 @@
-<table class="table table-striped" id="taxa">
-    @foreach ($taxa as $taxon)
-    <tr>
-        <td>
-            @if ($taxon->eol && !empty($taxon->eol->img))
-            <img src="{{ $taxon->eol->img }}" id="{{ $taxon->EOLid }}" class="thumb">
-            @else
-            <input type="hidden" name="noimg" value="{{ $taxon->EOLid }}" />
-            @endif
-        </td>
-        <td><a href="/taxon/{{ $taxon->taxonID }}">{{ $taxon->taxonID }}</td>
-        <td>{{ $taxon->EOLid }}</td>
-        <td>{{ $taxon->canonicalName }}</td>
-        <td>
-            @if ($taxon->eol && !empty($taxon->eol->jp))
-            {{ $taxon->eol->jp }}
-            @else
-            <input type="hidden" name="nojp" value="{{ $taxon->EOLid }}" />
-            @endif
-        </td>
-        <td>
-            @if ($taxon->eol && !empty($taxon->eol->en))
-            {{ $taxon->eol->en }}
-            @else
-            <input type="hidden" name="noen" value="{{ $taxon->EOLid }}" />
-            @endif
-        </td>
-        <td>{{ $taxon->taxonomicStatus }}</td>
-        <td>{{ $taxon->taxonRank }}</td>
-    </tr>
-    @endforeach
-</table>
+<div class="table-responsive">
+    <table class="table table-striped" id="taxa">
+        @foreach ($taxa as $taxon)
+        <tr>
+            <td>
+                @if ($taxon->eol && !empty($taxon->eol->img))
+                <img src="{{ $taxon->eol->img }}" id="{{ $taxon->EOLid }}" class="thumb">
+                @else
+                <input type="hidden" name="noimg" value="{{ $taxon->EOLid }}" />
+                @endif
+            </td>
+            <td><a href="/taxon/{{ $taxon->taxonID }}">{{ $taxon->taxonID }}</td>
+            <td>{{ $taxon->EOLid }}</td>
+            <td>{{ $taxon->canonicalName }}</td>
+            <td>
+                @if ($taxon->eol && !empty($taxon->eol->jp))
+                {{ $taxon->eol->jp }}
+                @else
+                <input type="hidden" name="nojp" value="{{ $taxon->EOLid }}" />
+                @endif
+            </td>
+            <td>
+                @if ($taxon->eol && !empty($taxon->eol->en))
+                {{ $taxon->eol->en }}
+                @else
+                <input type="hidden" name="noen" value="{{ $taxon->EOLid }}" />
+                @endif
+            </td>
+            <td>{{ $taxon->taxonomicStatus }}</td>
+            <td>{{ $taxon->taxonRank }}</td>
+        </tr>
+        @endforeach
+    </table>
+</div>
